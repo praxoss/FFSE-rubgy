09b1e33a432d966841467da991c2ae2e3056445b
praxoss <baptiste.prax@gmail.com>
2026-03-13 09:58:33 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -719,9 +719,9 @@ function MatchPage() {
                 {played ? (
                   <div className="relative">
                     {(!!(stats.home?.bonus_off) || !!(stats.home?.bonus_def)) && (
-                      <div className="absolute -top-3 -left-2 flex flex-col gap-0.5 z-10">
-                        {stats.home?.bonus_off && <span className="text-[12px] font-black text-white bg-[#DAB455] px-1 py-0 rounded font-mono leading-4">BO</span>}
-                        {stats.home?.bonus_def && <span className="text-[12px] font-black border border-ffse-navy text-ffse-navy bg-white px-1 py-0 rounded font-mono leading-4">BD</span>}
+                      <div className="absolute -top-2 -left-2 flex flex-col gap-0.5 z-10">
+                        {stats.home?.bonus_off && <span className="text-[15px] font-black text-white bg-[#DAB455] px-1 py-0 rounded font-mono leading-4">BO</span>}
+                        {stats.home?.bonus_def && <span className="text-[15px] font-black border border-ffse-navy text-ffse-navy bg-white px-1 py-0 rounded font-mono leading-4">BD</span>}
                       </div>
                     )}
                     <div className="bg-ffse-navy text-white px-6 py-3 rounded-2xl flex items-center gap-3 font-display text-4xl shadow-lg">
@@ -730,9 +730,9 @@ function MatchPage() {
                       <span className={match.score_away! >= match.score_home! ? "text-white" : "text-neutral-400"}>{match.score_away}</span>
                     </div>
                     {(!!(stats.away?.bonus_off) || !!(stats.away?.bonus_def)) && (
-                      <div className="absolute -top-3 -right-2 flex flex-col gap-0.5 z-10">
-                        {stats.away?.bonus_off && <span className="text-[12px] font-black text-white bg-[#DAB455] px-1 py-0 rounded font-mono leading-4">BO</span>}
-                        {stats.away?.bonus_def && <span className="text-[12px] font-black border border-ffse-navy text-ffse-navy bg-white px-1 py-0 rounded font-mono leading-4">BD</span>}
+                      <div className="absolute -top-2 -right-2 flex flex-col gap-0.5 z-10">
+                        {stats.away?.bonus_off && <span className="text-[15px] font-black text-white bg-[#DAB455] px-1 py-0 rounded font-mono leading-4">BO</span>}
+                        {stats.away?.bonus_def && <span className="text-[15px] font-black border border-ffse-navy text-ffse-navy bg-white px-1 py-0 rounded font-mono leading-4">BD</span>}
                       </div>
                     )}
                   </div>
